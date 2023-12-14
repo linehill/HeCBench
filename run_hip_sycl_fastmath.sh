be4762d679c5b4e3df3acf127db0ad5f0f858124
Michal Babej <michal.babej@intel.com>
2023-12-14 14:58:10 +0200

    run_hip_sycl_fastmath.sh

---
+++ run_hip_sycl_fastmath.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+LEVEL0=1
+OPENCL=0
+REPEATS=4
+
+export CHIP_L0_COLLECT_EVENTS_TIMEOUT=5
+export CHIP_L0_IMM_CMD_LISTS=1
+# export CHIP_DUMP_SPIRV=1
+# export SYCL_DUMP_IMAGES=1
+
+for D in $(echo *-hip *-sycl); do
+  mkdir -p $D/l0_cache
+done
+
+export CHIP_DEVICE_TYPE=gpu
+export CHIP_LOGLEVEL=crit
+
+# -------------------
+
+if [ $OPENCL -ne 0 ]; then
+
+  export CHIP_BE=opencl
+
+  ./scripts/autohecbench.py --warmup true --repeat ${REPEATS} --extra-compile-flags="-ffast-math" -o test_FULL_${REPEATS}_x_hip_oclBE.csv hip
+fi
+
+# -------------------
+
+if [ $LEVEL0 -ne 0 ]; then
+
+  export CHIP_BE=level0
+
+  ./scripts/autohecbench.py --warmup true --repeat ${REPEATS} --extra-compile-flags="-ffast-math" -o test_FULL_${REPEATS}_x_hip_l0BE.csv hip
+fi
+
+#############################################
+
+source /opt/intel/oneapi/setvars.sh
+
+if [ $OPENCL -ne 0 ]; then
+  export SYCL_DEVICE_FILTER=opencl:gpu:0
+  ./scripts/autohecbench.py --warmup true --repeat ${REPEATS} --extra-compile-flags="-fp-model=fast -fno-sycl-instrument-device-code" -o test_FULL_${REPEATS}_x_sycl_oclBE.csv --sycl-type opencl sycl
+fi
+
+if [ $LEVEL0 -ne 0 ]; then
+  export SYCL_DEVICE_FILTER=ext_oneapi_level_zero:gpu:0
+  ./scripts/autohecbench.py --warmup true --repeat ${REPEATS} --extra-compile-flags="-fp-model=fast -fno-sycl-instrument-device-code" -o test_FULL_${REPEATS}_x_sycl_l0BE.csv --sycl-type opencl sycl
+fi
